dbc013ee91a6ed8ee314a4b229b18707f9590563
yyluoyong <zhenyanxl@qq.com>
2017-01-29 19:41:45 +0800

    首次提交。

---
+++ clean.bat
@@ -0,0 +1,13 @@
+del *.aux  /s
+del *.bak  /s
+del *.bbl  /s
+del *.bcf  /s
+del *.blg  /s
+del *.gz   /s
+del *.gz(busy)  /s
+del *.log  /s
+del *.out  /s
+del *.synctex  /s
+del *.toc  /s
+del *.xdv  /s
+del *.xml  /s
